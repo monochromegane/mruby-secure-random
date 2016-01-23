94572700dc4728b2cdcc9176dc6f47dead0679c9
monochromegane <dev.kuro.obi@gmail.com>
2016-01-23 19:52:53 +0900

    Implemented a random_bytes method using /dev/urandom.

--- mrbgem.rake
+++ mrbgem.rake
@@ -1,4 +1,5 @@
 MRuby::Gem::Specification.new('mruby-secure-random') do |spec|
   spec.license = 'MIT'
   spec.authors = 'monochromegane'
+  spec.add_dependency('mruby-io')
 end
--- mrblib/mrb_securerandom.rb
+++ mrblib/mrb_securerandom.rb
@@ -1,5 +1,19 @@
 class SecureRandom
-  def bye
-    self.hello + " bye"
+  def self.random_bytes(n=nil)
+     n = n ? n.to_int : 16
+
+     begin
+       File.open("/dev/urandom", 'r') {|f|
+         ret = f.read(n)
+         unless ret.length == n
+           raise NotImplementedError, "Unexpected partial read from random device: only #{ret.length} for #{n} bytes"
+         end
+         return ret
+       }
+     rescue
+       raise File::NoFileError, "No random device"
+     end
+
+     raise NotImplementedError, "No random device"
   end
 end
