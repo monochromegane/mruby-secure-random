9fec85b7d71f8fcab7a7590d06bfb2a96a9e90a8
monochromegane <dev.kuro.obi@gmail.com>
2016-01-23 19:56:02 +0900

    Use class << self.

--- mrblib/mrb_securerandom.rb
+++ mrblib/mrb_securerandom.rb
@@ -1,23 +1,25 @@
 class SecureRandom
-  def self.random_bytes(n=nil)
-     n = n ? n.to_int : 16
+  class << self
+    def random_bytes(n=nil)
+      n = n ? n.to_int : 16
 
-     begin
-       File.open("/dev/urandom", 'r') {|f|
-         ret = f.read(n)
-         unless ret.length == n
-           raise NotImplementedError, "Unexpected partial read from random device: only #{ret.length} for #{n} bytes"
-         end
-         return ret
-       }
-     rescue
-       raise File::NoFileError, "No random device"
-     end
+      begin
+        File.open("/dev/urandom", 'r') {|f|
+          ret = f.read(n)
+          unless ret.length == n
+            raise NotImplementedError, "Unexpected partial read from random device: only #{ret.length} for #{n} bytes"
+          end
+          return ret
+        }
+      rescue
+        raise File::NoFileError, "No random device"
+      end
 
-     raise NotImplementedError, "No random device"
-  end
+      raise NotImplementedError, "No random device"
+    end
 
-  def self.hex(n=nil)
-    random_bytes(n).unpack("H*")[0]
+    def hex(n=nil)
+      random_bytes(n).unpack("H*")[0]
+    end
   end
 end
