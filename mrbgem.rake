09ee31cbea0d23bc3e497b9b2a4e9d46275ffa0a
monochromegane <dev.kuro.obi@gmail.com>
2016-01-23 22:29:31 +0900

    Added tests and dependencies to spec.

--- mrbgem.rake
+++ mrbgem.rake
@@ -2,4 +2,7 @@ MRuby::Gem::Specification.new('mruby-secure-random') do |spec|
   spec.license = 'MIT'
   spec.authors = 'monochromegane'
   spec.add_dependency('mruby-io')
+  spec.add_dependency('mruby-pack')
+  spec.add_dependency('mruby-string-ext')
+  spec.add_dependency('mruby-sprintf')
 end
--- test/mrb_securerandom.rb
+++ test/mrb_securerandom.rb
@@ -2,16 +2,37 @@
 ## SecureRandom Test
 ##
 
-assert("SecureRandom#hello") do
-  t = SecureRandom.new "hello"
-  assert_equal("hello", t.hello)
+class StubSecureRandom < SecureRandom
+  def self.random_bytes(n=nil)
+    if n == 16
+      "t\353\227\311\030p\214\337DJ1\325[\016BQ"
+    else
+      "\334"
+    end
+  end
 end
 
-assert("SecureRandom#bye") do
-  t = SecureRandom.new "hello"
-  assert_equal("hello bye", t.bye)
+assert("SecureRandom.random_bytes") do
+  assert_equal("\334", StubSecureRandom.random_bytes)
 end
 
-assert("SecureRandom.hi") do
-  assert_equal("hi!!", SecureRandom.hi)
+assert("SecureRandom.hex") do
+  assert_equal("dc", StubSecureRandom.hex)
+end
+
+assert("SecureRandom.base64") do
+  assert_equal("3A==", StubSecureRandom.base64)
+end
+
+assert("SecureRandom.urlsafe_base64") do
+  assert_equal("3A", StubSecureRandom.urlsafe_base64)
+  assert_equal("3A==", StubSecureRandom.urlsafe_base64(nil, true))
+end
+
+assert("SecureRandom.random_number with 1") do
+  assert_equal(0, StubSecureRandom.random_number(1))
+end
+
+assert("SecureRandom.uuid") do
+  assert_equal("74eb97c9-1870-4cdf-844a-31d55b0e4251", StubSecureRandom.uuid)
 end
